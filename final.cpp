e8657b5d7813fa37a5695e15e559b861312c7875
Cesar <ipog71@gmail.com>
2022-04-22 15:37:07 -0500

    first commit

---
+++ final.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+ 
+using namespace std;
+/*
+TODO:
+Menu para ver disponibilidad de mesas
+Menu para reservar mesa
+Pedir datos de los clientes
+Revisar si se puede reservar una mesa (Maquina finita)
+
+Letrero de si se reservo o no y por q
+*/
+
+int main()
+{
+    cout<<"Meme"; 
+
+    system("PAUSE");
+    return 0;
+}
